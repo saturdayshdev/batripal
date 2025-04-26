1913e05690736f0c0f421cac973f65f002907856
Oskar Wójcikiewicz <oskar@saturdaysheroes.dev>
2025-04-26 15:26:16 +0200

    feat(config): db schema

--- backend/src/config/config.schema.ts
+++ backend/src/config/config.schema.ts
@@ -8,10 +8,19 @@ export const ai = z.object({
   AI_TTS_VOICE: z.string().default('alloy'),
 });
 
+export const db = z.object({
+  DB_HOST: z.string().default('localhost'),
+  DB_PORT: z.coerce.number().default(5432),
+  DB_USER: z.string(),
+  DB_PASSWORD: z.string(),
+  DB_NAME: z.string(),
+});
+
 export const schema = z.object({
   APP_PORT: z.coerce.number().min(1000),
   NODE_ENV: z.enum(['development', 'production']).default('development'),
   ...ai.shape,
+  ...db.shape,
 });
 
 export type Env = z.infer<typeof schema>;
