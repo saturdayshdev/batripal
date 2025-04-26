b35a6be8579fc54b84e5bc503b398439a57a2fa4
Oskar Wójcikiewicz <oskar@saturdaysheroes.dev>
2025-04-26 16:49:38 +0200

    chore: cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,6 +4,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type,Accept,Authorization',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: false,
+  });
   app.useWebSocketAdapter(new IoAdapter(app));
   await app.listen(4040);
 }
