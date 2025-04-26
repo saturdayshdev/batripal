752e8b055d8023da6f1e9689da9dd48ce18070ef
Oskar Wójcikiewicz <oskar@saturdaysheroes.dev>
2025-04-26 14:00:14 +0200

    chore: remove mic service

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@config/config.module';
 import { AiModule } from '@ai/ai.module';
-import { MicService } from './mic.service';
 
 @Module({
   imports: [ConfigModule, AiModule],
-  providers: [MicService],
 })
 export class AppModule {}
