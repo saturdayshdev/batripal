957f8553a196591d1766d5149b1f7194573017f5
Oskar Wójcikiewicz <oskar@saturdaysheroes.dev>
2025-04-26 12:45:38 +0200

    feat(ai): service

---
+++ backend/src/ai/ai.service.ts
@@ -0,0 +1,47 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { ConfigService } from '@config/config.service';
+import { AiSdk } from './ai.sdk';
+import { AiPromptsService } from './ai.prompts';
+import { AiPrompt } from './ai.enum';
+import { createReadStream, type ReadStream } from 'fs';
+import * as path from 'path';
+import * as fs from 'fs/promises';
+import * as os from 'os';
+
+@Injectable()
+export class AiService {
+  private logger = new Logger(AiService.name);
+
+  constructor(
+    private config: ConfigService,
+    private aiSdk: AiSdk,
+    private aiPrompts: AiPromptsService,
+  ) {}
+
+  private async createTempFile(data: Buffer): Promise<ReadStream> {
+    const filePath = path.join(os.tmpdir(), `${Date.now()}.wav`);
+    await fs.writeFile(filePath, data);
+    return createReadStream(filePath);
+  }
+
+  async transcribe(buffer: Buffer): Promise<string | null> {
+    try {
+      const file = await this.createTempFile(buffer);
+      const response = await this.aiSdk.client.audio.transcriptions.create({
+        file: file,
+        model: this.config.env.AI_STT_MODEL,
+        language: this.config.env.AI_STT_LANGUAGE,
+        prompt: this.aiPrompts.getPrompt(AiPrompt.STT),
+        response_format: 'text',
+      });
+
+      await fs.unlink(file.path);
+      this.logger.log('Transcription successful');
+
+      return response;
+    } catch (error) {
+      this.logger.error('Error transcribing audio', error);
+      return null;
+    }
+  }
+}
